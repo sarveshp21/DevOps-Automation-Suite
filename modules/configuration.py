1eedac45dcef98f9e0bef2a4ddcc7e929a01ead2
Sarvesh <psarvesh267@gmail.com>
2026-09-10 23:20:49 +0530

    Module 2 update

--- modules/configuration.py
+++ modules/configuration.py
@@ -1,2 +1,56 @@
+# this file will be responsible for reading configuration files and providing configuration data to the rest of the application
+
+import json
+import yaml
+from pathlib import Path
+
 def run(args):
+    config = ConfigurationManager()
     print("Configuration module selected")
+    print("\nJSON Configuration")
+    print(config.load_json_config())
+    print("\nYAML Configuration")
+    print(config.load_yaml_config())
+
+class ConfigurationManager:
+    # constructor
+    def __init__(self):
+        # creates a Path object pointing to the config/ directory
+        self.config_dir = Path("config")
+        # creates the paths
+        self.json_config = self.config_dir / "config.json"
+        self.yaml_config = self.config_dir / "config.yaml"
+
+    def load_json_config(self):
+        # opens the config.json file in read mode
+        # converts json into a python dictionary
+        # if config.json does not exits, the program won't crash, it prints an error and returns an empty dictionary
+        # if json syntax is incorrect, it reports issue instead of terminating unexpectedly
+        # returning an empty dictionary allows the rest of the application to continue running safely, even if configuration file is missing or invalid
+        try: 
+            with open(self.json_config, "r") as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print(f"Error: {self.json_config} not found.")
+            return{}
+        except json.JSONDecodeError:
+            print(f"Error: Invalid JSON in {self.json_config}.")
+            return{}
+
+    def load_yaml_config(self):
+        # opens the config.yaml file in read mode
+        # converts yaml into a python dictionary
+        # if config.yaml does not exits, the program won't crash, it prints an error and returns an empty dictionary
+        # if yaml syntax is incorrect, it reports issue instead of terminating unexpectedly
+        # returning an empty dictionary allows the rest of the application to continue running safely, even if configuration file is missing or invalid
+        try:
+            with open(self.yaml_config, "r") as file:
+                return yaml.safe_load(file)
+        except FileNotFoundError:
+            print(f"Error: {self.yaml_config} not found.")
+            return {}
+        except yaml.YAMLError:
+            print(f"Error: Invalid YAML in {self.yaml_config}.")
+            return {}
+
+
--- modules/reporting.py
+++ modules/reporting.py
@@ -1,2 +1,58 @@
+import json
+import csv
+from pathlib import Path
+
 def run(args):
-    print("Reporting module selected")
+    report = ReportGenerator()
+    report.generate_txt_report()
+    print("Reports module selected")
+
+class ReportGenerator:
+    def __init__(self):
+        self.report_dir = Path("reports")
+
+        self.txt_dir = self.report_dir / "txt"
+        self.csv_dir = self.report_dir / "csv"
+        self.json_dir = self.report_dir / "json"
+
+        self.create_directories()
+
+    def create_directories(self):
+        self.txt_dir.mkdir(parents=True, exist_ok=True)
+        self.csv_dir.mkdir(parents=True, exist_ok=True)
+        self.json_dir.mkdir(parents=True, exist_ok=True)
+
+    def generate_txt_report(self, filename, content):
+        # creates report path, ex: reports/txt/system_report.txt
+        report_file = self.txt_dir/filename
+        # opens the file, "w" = write mode, creates the file if it doesn't exist, overwrites the file if it already exits
+        with open(report_file, "w") as file:
+            # writes the content
+            file.write(content)
+            # display success message
+            print(f"TXT report generated: {report_file}")
+
+    def generate_csv_report(self, filename, data):
+            # creates report path, ex: reports/csv/system_report.csv
+            report_file = self.csv_dir/filename
+            # opens the file, "w" = write mode, newline="" = prevents blank lines between rows, the writer object is responsible for writing rows into the csv file, creates the file if it doesn't exist, overwrites the file if it already exits
+            with open(report_file, "w", newline="") as file:
+                # creates the csv writer
+                writer = csv.writer(file)
+                # writes each row
+                for row in data:
+                    writer.writerow(row)
+                # display success message
+                print(f"CSV report generated: {report_file}")
+
+    def generate_json_report(self, filename, data):
+            # creates report path, ex: reports/json/system_report.json
+            report_file = self.json_dir/filename
+            # opens the file, "w" = write mode, creates the file if it doesn't exist, overwrites the file if it already exits
+            with open(report_file, "w") as file:
+                # writes the json data, indent=4 parameter formats the json nicely for readability
+                json.dump(data, file, indent=4)
+                # display success message
+                print(f"JSON report generated: {report_file}")
+
+    
